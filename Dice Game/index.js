b9d2ac6e9428581bad197dab6c04183bda9ffec8
Ramanpreet Singh <57689321+Ramanpreet9018@users.noreply.github.com>
2020-12-01 15:40:54 -0500

    updates index.js file

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,11 +1,11 @@
 /*----- For Dice #1 -----*/
 var randomNumber1 = Math.random();
 randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
-console.log(randomNumber1);
+//console.log(randomNumber1);
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //creating the image file-name for Dice #1
 
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "images/" + randomDiceImage; //extracting the image source for Dice #2
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
@@ -15,14 +15,14 @@ var randomNumber2 = Math.random();
 randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
 console.log(randomNumber2);
 
-var randomDiceImage = "dice" + randomNumber2 + ".png";
+var randomDiceImage = "dice" + randomNumber2 + ".png"; //creating the image file-name for Dice #1
 
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "images/" + randomDiceImage; //extracting the image source for Dice #2
 
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource);
 
-/*----- Comparison between both die -----*/
+/*----- Comparison between both die and displaying the winner -----*/
 if(randomNumber2 > randomNumber1) {
 	document.querySelector("h1").innerHTML = "Player2 Wins";
 } else if(randomNumber1 > randomNumber2) {
