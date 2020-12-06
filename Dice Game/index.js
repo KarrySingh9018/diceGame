abf0d7331f35e764395f02755309ca54164f2ff1
Ramanpreet Singh <57689321+KarrySingh9018@users.noreply.github.com>
2020-12-06 12:24:41 -0500

    Update index.js

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -5,7 +5,7 @@ randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //creating the image file-name for Dice #1
 
-var randomImageSource = "images/" + randomDiceImage; //extracting the image source for Dice #2
+var randomImageSource = "images/" + randomDiceImage; //extracting the image source for Dice #1
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
@@ -15,7 +15,7 @@ var randomNumber2 = Math.random();
 randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
 console.log(randomNumber2);
 
-var randomDiceImage = "dice" + randomNumber2 + ".png"; //creating the image file-name for Dice #1
+var randomDiceImage = "dice" + randomNumber2 + ".png"; //creating the image file-name for Dice #2
 
 var randomImageSource = "images/" + randomDiceImage; //extracting the image source for Dice #2
 
